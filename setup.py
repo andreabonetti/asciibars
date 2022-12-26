c78a9648fb1ebe5d95c3f3a8ca674f91f1e03fc4
Andrea Bonetti <andreabonetti@gmail.com>
2022-12-26 13:40:36 +0100

    setup.py : alpha version is 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'asciibars', 
-    version = '0.0.0',
+    version = '0.0.1',
     description = 'Bar charts in ASCII',
     py_modules = ["asciibars"],
     package_dir = {'':'src'},
